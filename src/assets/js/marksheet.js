d3c4eb353c80c4941bb322941dd2271229e132c8
Satish Kumar Sharma <skwebserv@gmail.com>
2024-05-25 23:50:06 +0530

    improve

--- src/assets/js/marksheet.js
+++ src/assets/js/marksheet.js
@@ -1,33 +1,35 @@
 import download from "downloadjs";
 import * as htmlToImage from "html-to-image";
 
-// var node = document.getElementById('my-node');
+document.addEventListener("DOMContentLoaded", function () {
+  const fileName = `Marksheet_${new Date().getTime()}`;
+  // var node = document.getElementById("my-node");
+  const targetElement = document.getElementById("marksheet");
 
-// htmlToImage.toPng(node)
-//   .then(function (dataUrl) {
-//     var img = new Image();
-//     img.src = dataUrl;
-//     document.body.appendChild(img);
-//   })
-//   .catch(function (error) {
-//     console.error('oops, something went wrong!', error);
-//   });
+  // htmlToImage.toPng(node)
+  //   .then(function (dataUrl) {
+  //     var img = new Image();
+  //     img.src = dataUrl;
+  //     document.body.appendChild(img);
+  //   })
+  //   .catch(function (error) {
+  //     console.error('oops, something went wrong!', error);
+  //   });
 
-// htmlToImage.toPng(document.getElementById("my-node")).then(function (dataUrl) {
-//   download(dataUrl, "my-node.png");
-// });
+  // htmlToImage.toPng(targetElement).then(function (dataUrl) {
+  //   download(dataUrl, fileName);
+  // });
 
-// htmlToImage
-//   .toJpeg(document.getElementById("my-node"), { quality: 0.95 })
-//   .then(function (dataUrl) {
-//     var link = document.createElement("a");
-//     link.download = "my-image-name.jpeg";
-//     link.href = dataUrl;
-//     link.click();
-//   });
+  htmlToImage.toJpeg(targetElement, { quality: 0.95 }).then(function (dataUrl) {
+    var link = document.createElement("a");
+    link.download = fileName;
+    link.href = dataUrl;
+    link.click();
+  });
 
-// htmlToImage
-//   .toCanvas(document.getElementById("my-node"))
-//   .then(function (canvas) {
-//     document.body.appendChild(canvas);
-//   });
+  // htmlToImage
+  //   .toCanvas(document.getElementById("my-node"))
+  //   .then(function (canvas) {
+  //     document.body.appendChild(canvas);
+  //   });
+});
